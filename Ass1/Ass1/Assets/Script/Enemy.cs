4756899dc2ec018b06be8085d041efc5d20fc412
minhndhe151002 <hichic375@gmail.com>
2023-06-17 23:37:57 +0700

    Thay đổi animation

--- Ass1/Ass1/Assets/Script/Enemy.cs
+++ Ass1/Ass1/Assets/Script/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ênmy : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float speed;
